637a80e994ac539c363d7a3d2afd0d22f02c7a31
Lorenzo <lorprofessional@gmail.com>
2022-07-01 15:45:47 +0200

    cambiamento 01_07_15:45

--- giochino_bello/src/App.js
+++ giochino_bello/src/App.js
@@ -2,16 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <h1>Il Gioco Inizia</h1>
       <div className="lettere_container">
-        <div><p></p></div>
-        <div><p></p></div>
-        <div><p></p></div>
-        <div><p></p></div>
-        <div><p></p></div>
-        <div><p></p></div>
+        <div><p><input type="text" maxLength="5" id="in1"/></p></div>
+        <div><p><input type="text" maxLength="5" id="in2"/></p></div>
+        <div><p><input type="text" maxLength="5" id="in3"/></p></div>
+        <div><p><input type="text" maxLength="5" id="in4"/></p></div>
+        <div><p><input type="text" maxLength="5" id="in5"/></p></div>
+        <div><p><input type="text" maxLength="5" id="in6"/></p></div>
       </div>
       <div className="gioco_container">
         <div></div>
@@ -44,6 +45,10 @@ function App() {
         <div></div>
         <div></div>
         <div></div>
+      </div>
+      <div className="buttons">
+        <button>cancella</button>
+        <button>invia</button>
       </div>
     </div>
   );
