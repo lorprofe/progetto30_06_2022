15ebcd603ad9c70ca533c69500289ab8ef0da66c
Lorenzo <lorprofessional@gmail.com>
2022-07-04 12:31:37 +0200

    parte_tecnica04_07_12:31

--- giochino_bello/src/App.js
+++ giochino_bello/src/App.js
@@ -11,60 +11,79 @@ function App() {
   const [inp6, setInp6] = useState();
   const [data, setData] = useState()
   axios.get("https://jsonplaceholder.typicode.com/todos/1")
+
     .then(res=>{
     }, [])
     .catch(err=>{console.log("Houston we've got a problem: " +err)});
-
+    
+    //targetizza l'innerHTML dell'input
+    function inp1Change(e){
+      setInp1(e.target.value)
+    }
+    function inp2Change(e){
+      setInp2(e.target.value)
+    }
+    function inp3Change(e){
+      setInp3(e.target.value)
+    }
+    function inp4Change(e){
+      setInp4(e.target.value)
+    }
+    function inp5Change(e){
+      setInp5(e.target.value)
+    }
+    function inp6Change(e){
+      setInp6(e.target.value)
+    }
   return (
     <>
       <div className="App">
         <h1>Il Gioco Inizia</h1>
         <div className="lettere_container">
-          <div><p><input type="text" maxLength="5" id="in1" value={inp1}/></p></div>
-          <div><p><input type="text" maxLength="5" id="in2" value={inp2}/></p></div>
-          <div><p><input type="text" maxLength="5" id="in3" value={inp3}/></p></div>
-          <div><p><input type="text" maxLength="5" id="in4" value={inp4}/></p></div>
-          <div><p><input type="text" maxLength="5" id="in5" value={inp5}/></p></div>
-          <div><p><input type="text" maxLength="5" id="in6" value={inp6}/></p></div>
+          <div><p><input type="text" maxLength="5" id="in1" onChange={inp1Change}/></p></div>
+          <div><p><input type="text" maxLength="5" id="in2" onChange={inp2Change}/></p></div>
+          <div><p><input type="text" maxLength="5" id="in3" onChange={inp3Change}/></p></div>
+          <div><p><input type="text" maxLength="5" id="in4" onChange={inp4Change}/></p></div>
+          <div><p><input type="text" maxLength="5" id="in5" onChange={inp5Change}/></p></div>
+          <div><p><input type="text" maxLength="5" id="in6" onChange={inp6Change}/></p></div>
         </div>
         <div className="gioco_container">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div id='dv1-1'></div>
+          <div id='dv1-2'></div>
+          <div id='dv1-3'></div>
+          <div id='dv1-4'></div>
+          <div id='dv1-5'></div>
+          <div id='dv2-1'></div>
+          <div id='dv2-2'></div>
+          <div id='dv2-3'></div>
+          <div id='dv2-4'></div>
+          <div id='dv2-5'></div>
+          <div id='dv3-1'></div>
+          <div id='dv3-2'></div>
+          <div id='dv3-3'></div>
+          <div id='dv3-4'></div>
+          <div id='dv3-5'></div>
+          <div id='dv4-1'></div>
+          <div id='dv4-2'></div>
+          <div id='dv4-3'></div>
+          <div id='dv4-4'></div>
+          <div id='dv4-5'></div>
+          <div id='dv5-1'></div>
+          <div id='dv5-2'></div>
+          <div id='dv5-3'></div>
+          <div id='dv5-4'></div>
+          <div id='dv5-5'></div>
+          <div id='dv6-1'></div>
+          <div id='dv6-2'></div>
+          <div id='dv6-3'></div>
+          <div id='dv6-4'></div>
+          <div id='dv6-5'></div>
         </div>
         <div className="buttons">
-          <button>invia</button>
+          <button id='btn_bello'>invia</button>
         </div>
       </div>
       <div className='prova_bella'>
-        {inp1}
       </div>
     </>
   );
